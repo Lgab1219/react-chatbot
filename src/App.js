eeff175ef444df0b877859a620211a7f637cf695
unknown <lgab1219>
2022-02-19 21:04:27 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [Str, addStr] = useState('');
+  const [messages, addMessage] = useState([]);
+  const [botMessages, setbotMessage] = useState([]);
+  
+
+  const sendInput = (e) => {
+      addStr(e.target.value);
+  }
+
+  const submitButton = () => {
+    
+    const details = {
+      id: Math.floor(Math.random() *10000),
+      string: Str,
+    }
+ 
+    //Adds an ID to every message on the array
+    addStr(details);
+
+    //Add messages from the string to the messages state array
+    addMessage([...messages, Str]);
+
+    //Add messages from the string to the botMessages state array
+    setbotMessage([...botMessages, Str]);
+ 
+  }
+
   return (
+    <> 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        messages.map((message) => (
+          <div className='message-tile'>
+            {/* Thanks to React.Fragment, it is posssible to add two
+            different elements inside a single div
+            - https://egghead.io/lessons/react-render-two-elements-side-by-side-with-react-fragments -*/}
+            <React.Fragment>
+              <h1 id='username'>Me</h1>
+              <p key={message.id} className='message-text'>{message}</p>
+              <h1 id='botname'>Bot</h1>
+              <p key={message.id} className='bot-message-text'>{message}</p>
+            </React.Fragment>
+          </div>
+        ))
+      }
+      <input type={'text'} className="input-bar" onChange={sendInput}></input>
+      <button className='submit' onClick={submitButton}></button>
     </div>
+    </>
   );
 }
 
